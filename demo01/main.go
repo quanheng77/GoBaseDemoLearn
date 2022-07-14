947b8a83f6d3d8388664c8dd99e5ac0a345bae38
李靖滨 <1732552078@qq.com>
2022-07-14 21:56:13 +0800

    上传demo01

---
+++ demo01/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Demo 01  hello world 案例
+
+func main() {
+	fmt.Println("Hello World,Golang!!!")
+}
+
+//fmt 包  实例化I/O
+
+/**
+General：
+	%v :当前值
+	%T :当前值属性
+Boolean：
+	%t: 判断对错
+Integer:
+	%d	十进制地址值
+	%o	八进制地址值
+	%O	0o为前缀的地址值
+
+*/
